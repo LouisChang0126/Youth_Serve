ac825b40f2665a314f2c544f93397b28a6664526
LouisChang0126 <qulip.louis.0126@gmail.com>
2023-12-19 15:38:57 +0800

    '2024v1'

--- firestore.js
+++ firestore.js
@@ -19,39 +19,76 @@ document.addEventListener("DOMContentLoaded", function() {
     db.collection("serve").get().then((querySnapshot) => {
         querySnapshot.forEach((doc) => {
             if (doc.exists) {
-                // 获取文档数据
-                const data = doc.data();
-                //hightlight
-                const leader = (data.主領 === user) ? 'class="is-warning"' : '';
-                const vice = (data.副主領 === user) ? 'class="is-warning"' : '';
-                const vocal = (data.助唱.includes(user)) ? 'class="is-warning"' : '';
-                const piano = (data.司琴 === user) ? 'class="is-warning"' : '';
-                const drum = (data.鼓手 === user) ? 'class="is-warning"' : '';
-                const mixer = (data.音控.includes(user)) ? 'class="is-warning"' : '';
-                const reminder = (data.提醒人 === user) ? 'class="is-warning"' : '';
-                const ppt = (data.字幕.includes(user)) ? 'class="is-warning"' : '';
-                const anchor = (data.司會 === user) ? 'class="is-warning"' : '';
-                const offering = (data.奉獻 === user) ? 'class="is-warning"' : '';
-                const welcomer = (data.招待.includes(user)) ? 'class="is-warning"' : '';
-                const prayer = (data["會前(後)"].includes(user)) ? 'class="is-warning"' : '';
-                //內文
-                document.getElementById('chart').innerHTML += `
-                <tr>
-                    <th>${doc.id.substring(5,10).replace('.', '/')}</th>
-                    <th ${leader}>${data.主領}</th>
-                    <th ${vice}>${data.副主領}</th>
-                    <th ${vocal}>${data.助唱}</th>
-                    <th ${piano}>${data.司琴}</th>
-                    <th ${drum}>${data.鼓手}</th>
-                    <th ${mixer}>${data.音控}</th>
-                    <th ${reminder}>${data.提醒人}</th>
-                    <th ${ppt}>${data.字幕}</th>
-                    <th ${anchor}>${data.司會}</th>
-                    <th ${offering}>${data.奉獻}</th>
-                    <th ${welcomer}>${data.招待}</th>
-                    <th ${prayer}>${data["會前(後)"]}</th>
-                </tr>
-                `;
+                if(doc.id.substring(0,4)=="2023"){
+                    // 获取文档数据
+                    const data = doc.data();
+                    //hightlight
+                    const leader = (data.主領 === user) ? 'class="is-warning"' : '';
+                    const vice = (data.副主領 === user) ? 'class="is-warning"' : '';
+                    const vocal = (data.助唱.includes(user)) ? 'class="is-warning"' : '';
+                    const piano = (data.司琴 === user) ? 'class="is-warning"' : '';
+                    const drum = (data.鼓手 === user) ? 'class="is-warning"' : '';
+                    const mixer = (data.音控.includes(user)) ? 'class="is-warning"' : '';
+                    const reminder = (data.提醒人 === user) ? 'class="is-warning"' : '';
+                    const ppt = (data.字幕.includes(user)) ? 'class="is-warning"' : '';
+                    const anchor = (data.司會 === user) ? 'class="is-warning"' : '';
+                    const offering = (data.奉獻 === user) ? 'class="is-warning"' : '';
+                    const welcomer = (data.招待.includes(user)) ? 'class="is-warning"' : '';
+                    const prayer = (data["會前(後)"].includes(user)) ? 'class="is-warning"' : '';
+                    //內文
+                    document.getElementById('chart').innerHTML += `
+                    <tr>
+                        <th>${doc.id.substring(5,10).replace('.', '/')}</th>
+                        <th ${leader}>${data.主領}</th>
+                        <th ${vice}>${data.副主領}</th>
+                        <th ${vocal}>${data.助唱}</th>
+                        <th ${piano}>${data.司琴}</th>
+                        <th ${drum}>${data.鼓手}</th>
+                        <th ${mixer}>${data.音控}</th>
+                        <th ${reminder}>${data.提醒人}</th>
+                        <th ${ppt}>${data.字幕}</th>
+                        <th ${anchor}>${data.司會}</th>
+                        <th ${offering}>${data.奉獻}</th>
+                        <th ${welcomer}>${data.招待}</th>
+                        <th ${prayer}>${data["會前(後)"]}</th>
+                    </tr>
+                    `;
+                }
+                else if(doc.id.substring(0,4)=="2024"){
+                    // 获取文档数据
+                    const data = doc.data();
+                    //hightlight
+                    const leader = (data.主領 === user) ? 'class="is-warning"' : '';
+                    const vice = (data.副主領 === user) ? 'class="is-warning"' : '';
+                    const vocal = (data.助唱.includes(user)) ? 'class="is-warning"' : '';
+                    const piano = (data.司琴 === user) ? 'class="is-warning"' : '';
+                    const drum = (data.鼓手 === user) ? 'class="is-warning"' : '';
+                    const mixer = (data.音控.includes(user)) ? 'class="is-warning"' : '';
+                    const reminder = (data.提醒人 === user) ? 'class="is-warning"' : '';
+                    const ppt = (data.字幕.includes(user)) ? 'class="is-warning"' : '';
+                    const anchor = (data.司會 === user) ? 'class="is-warning"' : '';
+                    const offering = (data.奉獻 === user) ? 'class="is-warning"' : '';
+                    const welcomer = (data.招待.includes(user)) ? 'class="is-warning"' : '';
+                    const prayer = (data["會前"].includes(user)) ? 'class="is-warning"' : '';
+                    //內文
+                    document.getElementById('chart').innerHTML += `
+                    <tr>
+                        <th>${doc.id.substring(5,10).replace('.', '/')}</th>
+                        <th ${leader}>${data.主領}</th>
+                        <th ${vice}>${data.副主領}</th>
+                        <th ${vocal}>${data.助唱}</th>
+                        <th ${piano}>${data.司琴}</th>
+                        <th ${drum}>${data.鼓手}</th>
+                        <th ${mixer}>${data.音控}</th>
+                        <th ${reminder}>${data.提醒人}</th>
+                        <th ${ppt}>${data.字幕}</th>
+                        <th ${anchor}>${data.司會}</th>
+                        <th ${offering}>${data.奉獻}</th>
+                        <th ${welcomer}>${data.招待}</th>
+                        <th ${prayer}>${data["會前"]}</th>
+                    </tr>
+                    `;
+                }
             }
         });
     });
